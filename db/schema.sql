cfb2e44afc09a7fe59c547b580615eccd7b6a875
rudyxwhite <brandonrwing98@gmail.com>
2023-03-09 07:19:02 -0600

    required necessary packages and created the tables in the schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,31 @@
-this will be the schema file
+DROP DATABASE IF EXISTS employeetracker_db;
+
+CREATE DATABASE employeetracker_db;
+
+USE employeetracker_db;
+
+CREATE TABLE department(
+    id INT NOT NULL PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+)
+
+CREATE TABLE role(
+    id INT NOT NULL PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+)
+
+CREATE TABLE employee(
+    id INT NOT NULL PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT NOT NULL,
+    FOREIGN KEY(role_id)
+    REFERENCES role(id)
+    FOREIGN KEY(manager_id)
+    REFERENCES employee(id)
+)
