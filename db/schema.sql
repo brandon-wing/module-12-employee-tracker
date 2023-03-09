c5f1561bb93bac13fcf196bfe77b84fa26af1aa1
rudyxwhite <brandonrwing98@gmail.com>
2023-03-09 17:47:31 -0600

    added functions for all the desired tasks, but only have the prompts so far.

--- db/schema.sql
+++ db/schema.sql
@@ -4,28 +4,29 @@ CREATE DATABASE employeetracker_db;
 
 USE employeetracker_db;
 
-CREATE TABLE department(
-    id INT NOT NULL PRIMARY KEY,
-    name VARCHAR(30) NOT NULL
-)
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY(id)
+);
 
-CREATE TABLE role(
+CREATE TABLE role (
     id INT NOT NULL PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INT NOT NULL,
     FOREIGN KEY (department_id)
     REFERENCES department(id)
-)
+);
 
-CREATE TABLE employee(
+CREATE TABLE employee (
     id INT NOT NULL PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
     manager_id INT NOT NULL,
     FOREIGN KEY(role_id)
-    REFERENCES role(id)
+    REFERENCES role(id),
     FOREIGN KEY(manager_id)
     REFERENCES employee(id)
-)
+);
--- index.js
+++ index.js
@@ -1,1 +1,83 @@
-//this will be the index.js
+const inquirer = require("inquirer")
+const mysql = require('mysql2');
+const cTable = require('console.table');
+const db = require('.')
+//all the packages required are above!
+
+// create the connection to database
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Mb0217nt!',
+    database: 'employeetracker_db'
+  });
+  //Welcome/entry screen with the first set of questions.
+  function welcomePrompt(){
+    inquirer.prompt = {
+    type: 'list',
+    message: 'Welcome to the Employee tracker! Please choose a following option.',
+    name:'welcome',
+    choices: [
+    'View all departments',
+    'View all roles',
+    'View all employees',
+    'Add a department',
+    'Add a role',
+    'Add an employee',
+    'Update an employee role',
+    'Nothing']
+}}
+
+
+  function addDepartment(){
+  [{
+    type: 'input',
+    message: 'What is the name of the department you would like to add?',
+    name: 'department'
+  }]};
+
+  function addRole(){
+    [{
+    type: 'input',
+    message: 'What is the name of the role that you would like to add?',
+    name: 'role'
+  },
+  {
+    type: 'input',
+    message: 'What is the salary for the role?',
+    name: 'roleSalary'
+  },
+  {
+    type: 'input',
+    message: 'Which department is this role employed in?',
+    name: 'roleDepartment'
+  }]};
+
+  function addEmployee() {
+  [{
+    type: 'input',
+    message: 'What is the first name of the employee?',
+    name: 'firstname'
+  },
+  {
+    type: 'input',
+    message: 'What is the last name of the employee?',
+    name: 'lastname'
+  },
+  {
+    type: 'input',
+    message: 'What is the role of the employee?',
+    name: 'employeeRole'
+  },
+  {
+    type: 'input',
+    message: 'Who is the manager of the employee?',
+    name: 'employeeManager'
+  }]};
+
+  function updateRole(){
+  [{
+    type: 'input',
+    message: 'Whose role would you like to update?',
+    name: 'updateRole'
+  }]};
