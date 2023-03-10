27f816aa81312463455c3a24ce52b58e03b42e59
rudyxwhite <brandonrwing98@gmail.com>
2023-03-09 18:39:34 -0600

    added inquirer.prompt to the functions

--- index.js
+++ index.js
@@ -13,71 +13,73 @@ const connection = mysql.createConnection({
   });
   //Welcome/entry screen with the first set of questions.
   function welcomePrompt(){
-    inquirer.prompt = {
-    type: 'list',
-    message: 'Welcome to the Employee tracker! Please choose a following option.',
-    name:'welcome',
-    choices: [
-    'View all departments',
-    'View all roles',
-    'View all employees',
-    'Add a department',
-    'Add a role',
-    'Add an employee',
-    'Update an employee role',
-    'Nothing']
-}}
+    inquirer.prompt({
+      type: 'list',
+      message: 'Welcome to the Employee tracker! Please choose a following option.',
+      name:'welcome',
+      choices: [
+      'View all departments',
+      'View all roles',
+      'View all employees',
+      'Add a department',
+      'Add a role',
+      'Add an employee',
+      'Update an employee role',
+      'Nothing']}
+)};
 
 
   function addDepartment(){
-  [{
-    type: 'input',
-    message: 'What is the name of the department you would like to add?',
-    name: 'department'
-  }]};
+    inquirer.prompt([{
+      type: 'input',
+      message: 'What is the name of the department you would like to add?',
+      name: 'department'
+  }])};
 
   function addRole(){
-    [{
-    type: 'input',
-    message: 'What is the name of the role that you would like to add?',
-    name: 'role'
-  },
-  {
-    type: 'input',
-    message: 'What is the salary for the role?',
-    name: 'roleSalary'
-  },
-  {
-    type: 'input',
-    message: 'Which department is this role employed in?',
-    name: 'roleDepartment'
-  }]};
+    inquirer.prompt(
+      {
+      type: 'input',
+      message: 'What is the name of the role that you would like to add?',
+      name: 'role'
+       },
+       {
+      type: 'input',
+      message: 'What is the salary for the role?',
+      name: 'roleSalary'
+       },
+       {
+      type: 'input',
+      message: 'Which department is this role employed in?',
+      name: 'roleDepartment'
+    }
+  )};
 
   function addEmployee() {
-  [{
-    type: 'input',
-    message: 'What is the first name of the employee?',
-    name: 'firstname'
+  inquirer.prompt({
+      type: 'input',
+      message: 'What is the first name of the employee?',
+      name: 'firstname'
   },
   {
-    type: 'input',
-    message: 'What is the last name of the employee?',
-    name: 'lastname'
+      type: 'input',
+      message: 'What is the last name of the employee?',
+      name: 'lastname'
   },
   {
-    type: 'input',
-    message: 'What is the role of the employee?',
-    name: 'employeeRole'
+      type: 'input',
+      message: 'What is the role of the employee?',
+      name: 'employeeRole'
   },
   {
-    type: 'input',
-    message: 'Who is the manager of the employee?',
-    name: 'employeeManager'
-  }]};
+      type: 'input',
+      message: 'Who is the manager of the employee?',
+      name: 'employeeManager'
+  })};
 
   function updateRole(){
-  [{
-    type: 'input',
-    message: 'Whose role would you like to update?',
-    name: 'updateRole'
-  }]};
+  inquirer.prompt({
+      type: 'input',
+      message: 'Whose role would you like to update?',
+      name: 'updateRole'
+  })};
